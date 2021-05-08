bd2bcd8f5c80e2583bd0ff26531e8559bf6e115f
Halahamdy22 <halahamdyabdelhady@gmail.com>
2021-05-09 00:13:07 +0200

    Initial Files

---
+++ crawler.java
@@ -0,0 +1,8 @@
+
+public class crawler{
+
+
+    public static void main(String[] args) {
+        System.out.println("I am the crawler ");
+    }
+}
---
+++ indexer.java
@@ -0,0 +1,8 @@
+public class indexer {
+   
+    public static void main(String[] args) {
+        System.out.println("I am the indexer ");
+    }
+}
+
+
